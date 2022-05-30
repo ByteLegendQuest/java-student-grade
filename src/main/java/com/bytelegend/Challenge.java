aaca39c855afb1476b956105774238f468061348
chinahzh <2987140244@qq.com>
2022-05-30 01:45:00 +0000

    Update at 2022-05-30T01:45:00.070240Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -8,23 +8,35 @@ public class Challenge {
         System.out.println(getStudentGrade(75));
         System.out.println(getStudentGrade(100));
     }
-
+    //
     /**
      * `getStudentGrade(int score)` returns the grade of the student based on the given score.
      *
-     * <p>If 0<=score<60, return `E`;
+     * <p>If , return `E`;
      *
-     * <p>If 60<=score<70, return `D`;
+     * <p>If , return `D`;
      *
-     * <p>If 70<=score<80, return `C`;
+     * <p>If , return `C`;
      *
-     * <p>If 80<=score<90, return `B`;
+     * <p>If , return `B`;
      *
-     * <p>If 90<=score<=100, return `A`;
+     * <p>If , return `A`;
      *
      * <p>If the score is not in 0-100 range, return `Invalid Score`.
      */
     public static String getStudentGrade(int score) {
-        return null;
+        
+        if(90<=score<=100)
+            return A;
+        else if(80<=score<90)
+            return B;
+        else if(70<=score<80)
+            return C;
+         else if(60<=score<70)
+            return D;
+        else if(0<=score<60)
+            return E;
+        else
+            return "Invalid Score"
     }
 }
