f27f2c60d2ac85f854d327b670aa9ad5097b078d
SpaceTimeAdministrator <106767177+SpaceTimeAdministrator@users.noreply.github.com>
2022-06-12 12:36:53 +0000

    Update at 2022-06-12T12:36:53.337017Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -24,32 +24,6 @@ public class Challenge {
      *
      * <p>If the score is not in 0-100 range, return `Invalid Score`.
      */
-    package com.bytelegend;
-
-public class Challenge {
-    public static void main(String[] args) {
-        System.out.println(getStudentGrade(-1));
-        System.out.println(getStudentGrade(0));
-        System.out.println(getStudentGrade(60));
-        System.out.println(getStudentGrade(75));
-        System.out.println(getStudentGrade(100));
-    }
-
-    /**
-     * `getStudentGrade(int score)` returns the grade of the student based on the given score.
-     *
-     * <p>If 0<=score<60, return `E`;
-     *
-     * <p>If 60<=score<70, return `D`;
-     *
-     * <p>If 70<=score<80, return `C`;
-     *
-     * <p>If 80<=score<90, return `B`;
-     *
-     * <p>If 90<=score<=100, return `A`;
-     *
-     * <p>If the score is not in 0-100 range, return `Invalid Score`.
-     */
     public static String getStudentGrade(int score) {
         if (score >= 0 && score < 60) {
             return "E";
@@ -65,6 +39,4 @@ public class Challenge {
             return "Invalid Score";
         }
     }
-}
-
 }
