5a72d9306f2f5458ffbd4e594df4265c23799717
Birlinha <omenaribeiro13@gmail.com>
2022-07-15 14:24:35 +0000

    Update at 2022-07-15T14:24:35.675255Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -25,17 +25,18 @@ public class Challenge {
      * <p>If the score is not in 0-100 range, return `Invalid Score`.
      */
     public static String getStudentGrade(int score) {
-        return score <= 60
-        ? "E"
-        : score<70
-        ? "D"
-        : score<80
-        ? "C"
-        : score<90
-        ? "B"
-        : score <=100
-        ? "A"
-        : score == null
-        ? "Invalid Score";
+        if(score<0) {
+            return "Invalid Score";
+        } else if (score <= 60) {
+            return "E";
+        } else if (score < 70) {
+            return "D";
+        } else if (score < 80) {
+            return "C";
+        } else if (score < 90) {
+            return "B";
+        } else if (score <= 100) {
+            return "A";
+        }
     }
 }
