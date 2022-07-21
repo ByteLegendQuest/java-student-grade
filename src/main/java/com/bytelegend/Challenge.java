3534328b449049c4340eeafba16771cbf4e7747b
GOH73 <goh73@outlook.com>
2022-07-21 07:03:27 +0000

    Update at 2022-07-21T07:03:27.882951Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -25,6 +25,18 @@ public class Challenge {
      * <p>If the score is not in 0-100 range, return `Invalid Score`.
      */
     public static String getStudentGrade(int score) {
-        return null;
+        if (score > 100 || score < 0) {
+            return "Invalid Score";
+        } else if (score < 60) {
+            return "E";
+        } else if (score < 70) {
+            return "D";
+        } else if (score < 80) {
+            return "C";
+        } else if (score < 90) {
+            return "B";
+        } else {
+            return "A";
+        }
     }
 }
