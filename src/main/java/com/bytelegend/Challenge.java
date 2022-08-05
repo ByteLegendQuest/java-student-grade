174f86f8a19c124aa8a58dab83e021381b3281d4
TaoYang1998 <1121758475@qq.com>
2022-08-05 13:16:24 +0000

    Update at 2022-08-05T13:16:24.162655Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -25,6 +25,26 @@ public class Challenge {
      * <p>If the score is not in 0-100 range, return `Invalid Score`.
      */
     public static String getStudentGrade(int score) {
-        return null;
+         if (score >= 0 && score < 60){
+            return "E";
+        }
+
+        if (score >= 60 && score < 70){
+            return "D";
+        }
+
+        if (score >= 70 && score < 80){
+            return "C";
+        }
+
+        if (score >= 80 && score < 90){
+            return "B";
+        }
+
+        if (score >= 90 && score <= 100){
+            return "A";
+        }
+
+        return "Invalid Score";
     }
 }
