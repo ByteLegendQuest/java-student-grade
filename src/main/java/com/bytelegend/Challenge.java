d6f81fe7e71f44dc76611b434098df178c46a6f3
ZusmanFate <demonsLwg@foxmail.com>
2022-09-06 04:00:55 +0000

    Update at 2022-09-06T04:00:55.533823Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -27,15 +27,15 @@ public class Challenge {
     public static String getStudentGrade(int score) {
         if (score < 60 && score >= 0) {
             return "E";
-        } else if (score < 70) {
+        } else if (score < 70 && score >= 60) {
             return "D";
-        } else if (score < 80) {
+        } else if (score >= 70 && score < 80) {
             return "C";
-        } else if (score < 90) {
+        } else if (score >= 80 && score < 90) {
             return "B";
-        } else if (score <= 100) {
+        }else if (score >= 90 && score <= 100) {
             return "A";
-        } else {
+        }else {
             return "Invalid Score";
         }
     }
