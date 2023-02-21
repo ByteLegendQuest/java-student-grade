cdd8ba41adb67f531ad749b2dbb1e9f61a655d8e
darqlt <stungys@gmail.com>
2023-02-21 17:17:04 +0000

    Update at 2023-02-21T17:17:04.864627Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -9,21 +9,6 @@ public class Challenge {
         System.out.println(getStudentGrade(100));
     }
 
-    /**
-     * `getStudentGrade(int score)` returns the grade of the student based on the given score.
-     *
-     * <p>If 0<=score<60, return `E`;
-     *
-     * <p>If 60<=score<70, return `D`;
-     *
-     * <p>If 70<=score<80, return `C`;
-     *
-     * <p>If 80<=score<90, return `B`;
-     *
-     * <p>If 90<=score<=100, return `A`;
-     *
-     * <p>If the score is not in 0-100 range, return `Invalid Score`.
-     */
     public static String getStudentGrade(int score) {
         if score > 100 return "Invalid Score";
         if score >= 90 return "A";
