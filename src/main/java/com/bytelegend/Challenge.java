aae2fd5cb03c3bf8682fd2dd5850d9f211c43305
darqlt <stungys@gmail.com>
2023-02-21 17:19:55 +0000

    Update at 2023-02-21T17:19:55.767008Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -10,12 +10,12 @@ public class Challenge {
     }
 
     public static String getStudentGrade(int score) {
-        if score > 100 return "Invalid Score";
-        if score >= 90 return "A";
-        if score >= 80 return "B";
-        if score >= 70 return "C";
-        if score >= 60 return "D";
-        if score >= 0 return "E";
+        if (score > 100) return "Invalid Score";
+        if (score >= 90) return "A";
+        if (score >= 80) return "B";
+        if (score >= 70) return "C";
+        if (score >= 60) return "D";
+        if (score >= 0) return "E";
         return "Invalid Score";
     }
 }
