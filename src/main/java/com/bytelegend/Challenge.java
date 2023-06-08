4d2c3b65dc60d4c2a5bf079b0ed877f2875df134
Windsority <mcfengxin@gmail.com>
2023-06-08 22:03:41 +0000

    Update at 2023-06-08T22:03:41.499883Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -25,6 +25,12 @@ public class Challenge {
      * <p>If the score is not in 0-100 range, return `Invalid Score`.
      */
     public static String getStudentGrade(int score) {
-        return null;
+        if (score < 0) return "Invalid Score";
+        if (score < 60) return "E";
+        if (score < 70) return "D";
+        if (score < 80) return "C";
+        if (score < 90) return "B";
+        if (score <= 100) return "A";
+        return "Invalid Score";
     }
 }
